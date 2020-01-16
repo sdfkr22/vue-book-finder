385b48ae9054f0a5749f88255c78abeee8316074
Sedef Kara <sedefakkoc@gmail.com>
2020-01-16 16:02:07 +0300

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: 'vue-book-finder/dist/',
     filename: 'build.js'
   },
   module: {
